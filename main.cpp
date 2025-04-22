d611f550ffccb1b0b1454d21192345b99c887088
Wily2024 <jbardales1310@gmail.com>
2025-04-22 13:47:37 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,15 @@
-#include <iostream> // cout
-#include "demos.h"   // para usar la funcion suma, resta
-#include "array.h"
-#include "struct.h"
-#include "recursion.h"
-#include "sorting.h"
-using namespace std; // Para evitar el std::
 
-// Para compilar con C++:
-// clang++ -o main main.cpp arit.cpp demos.cpp array.cpp
-//
-// Para compilar con C++17:
-// g++ -pthread -std=c++17 -o main main.cpp arit.cpp demos.cpp array.cpp struct.cpp recursion.cpp
-// para correr ejecutar: ./main
+#include <iostream>
+#include "invertir_palabra.h"
+using namespace std;
 
-int main(){
-    cout << "Fundamentos de Programación 2025-I" << endl;
-    // DemoFunciones();
-    // DemoParametros();
-    // DemoArrays();
-    // DemoStructs();
-    // DemoRecursividad();
-    DemoSorting();
+int main() {
+    char palabra[] = "Hola";  
+    cout << "Palabra original: " << palabra << endl;
+
+    invertirPalabra(palabra);
+
+    cout << "Palabra invertida: " << palabra << endl;
+
     return 0;
 }
-
-
---
+++ string.cpp
@@ -0,0 +1,16 @@
+
+#include "invertir_palabra.h"
+#include <cstring>  
+using namespace std;
+
+void invertirPalabra(char* palabra) {
+    int longitud = strlen(palabra);  
+    char *inicio = palabra;              
+    char *fin = palabra + longitud - 1;  
+    
+    while (inicio < fin) {
+        swap(*inicio, *fin);  
+        inicio++;
+        fin--;
+    }
+}
