db0b637ff2d4458599a14c2f563eac29f19fdb1d
Wily2024 <jbardales1310@gmail.com>
2025-04-22 13:48:57 -0500

    string.cpp

--- string.cpp
+++ string.cpp
@@ -3,7 +3,7 @@
 #include <cstring>  
 using namespace std;
 
-void invertirPalabra(char* palabra) {
+void invertirPalabra(char *palabra) {
     int longitud = strlen(palabra);  
     char *inicio = palabra;              
     char *fin = palabra + longitud - 1;  
