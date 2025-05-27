f31a3bc2c0bb8e54467acb3e24944de3193e7a91
Motita <miguelandrenietoguillen@gmail.com>
2025-05-27 15:35:22 -0500

    Operadores listos, las referencias, el destructor, etc ... aún faltan algunas operaciones

--- complex.h
+++ complex.h
@@ -7,17 +7,52 @@ using ImagType = double;
 class Complex
 {
 private:
-    // Real part
-    // RealType m_r = 0;
-    // Real part
-    // ImagType m_i = 0;
+   
+   RealType m_r = 0;
+   ImagType m_i = 0;
+
 public:
-    // Constructores
-
-    // Destructor
-
-    // Operadores
+    // Constructor
+    Complex() = default;                         
+    Complex(RealType r, ImagType i) : m_r(r), m_i(i) {} 
+    Complex(const Complex& other) = default; 
     
+    // Destructor
+    ~Complex() = default; //uso el destructor por defecto
+    
+    // Operadores
+    Complex& operator=(const Complex& other) = default;
+
+    // Operador suma
+    Complex operator+(const Complex& other) const {
+        return Complex(m_r + other.m_r, m_i + other.m_i);
+    }
+
+    // Operador resta
+    Complex operator-(const Complex& other) const {
+        return Complex(m_r - other.m_r, m_i - other.m_i);
+    }
+
+    // Operador multiplicación
+    Complex operator*(const Complex& other) const {
+        return Complex(
+            m_r * other.m_r - m_i * other.m_i,
+            m_r * other.m_i + m_i * other.m_r
+        );
+    }
+
+    // Operador igualdad
+    bool operator==(const Complex& other) const {
+        return (m_r == other.m_r) && (m_i == other.m_i);
+    }
+
+    bool operator!=(const Complex& other) const {
+        return !(*this == other);
+    }
+
+    RealType real() const { return m_r; }
+    ImagType imag() const { return m_i; }
+
 };
 
 #endif // __COMPLEX_H__
