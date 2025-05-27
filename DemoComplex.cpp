74ec76b5ac71a98995ca39b6e3e7cf1e2675d8c2
FabricioYV <fabricioyauri0903@gmail.com>
2025-05-27 16:19:50 -0500

    task solved

--- DemoComplex.cpp
+++ DemoComplex.cpp
@@ -5,20 +5,19 @@ using namespace std;
 
 void DemoComplex(){
     cout << "DemoComplex ..." << endl;
-    // Complex c1, c2,
-    //         c3(2, 5), 
-    //         c4(-3, 7), 
-    //         c5(-2, -4),
-    //         c6(1, 1);
+     Complex c1, c2,
+             c3(2, 5), 
+             c4(-3, 7), 
+             c5(-2, -4),
+             c6(1, 1);
 
     // // +, -, *, /, +=, =-, *=, /=, <<, >>, =, operator RealType, operator ImagType
-    // c1 = c3 * c4;
-    // c1 += c5;
-    // c1 += Complex(5, 0); 
-    // c1 -= c6;
-    // c1 = c2 / c6 + c3 - c4 * c5
-    // cout << "Mi Complejo: " << c1 << " ..." << endl;
+     c1 = c3 * c4;
+     c1 += c5;
+     c1 += Complex(5, 0); 
+     c1 -= c6;
+     c1 = c2 / c6 + c3 - c4 * c5;
+     cout << "Mi Complejo: " << c1 << " ..." << endl;
 
-    // RealType r = (RealType)c1;  // c1.getReal();
-    // ImagType i = (ImagType)c1;  // c1.getImag();
+    
 }
--- complex.h
+++ complex.h
@@ -1,6 +1,8 @@
 #ifndef __COMPLEX_H__
 #define __COMPLEX_H__
-
+#include <cmath>
+#include <iostream>
+using namespace std;
 using RealType = double;
 using ImagType = double;
 
@@ -8,16 +10,81 @@ class Complex
 {
 private:
     // Real part
-    // RealType m_r = 0;
+     RealType m_r = 0;
     // Real part
-    // ImagType m_i = 0;
+     ImagType m_i = 0;
 public:
     // Constructores
-
+    Complex() = default; // Default constructor
+    Complex(RealType r, ImagType i) : m_r(r), m_i(i) {} // Parameterized constructor
+    
     // Destructor
 
+    ~Complex() = default; // Default destructor
+    
     // Operadores
     
-};
+    Complex& operator+=(const Complex& other){
+        m_r += other.getReal();
+        m_i += other.getImag();
+        return *this;
+    }
+    Complex& operator-=(const Complex& other){
+        m_r -= other.getReal();
+        m_i -= other.getImag();
+        return *this;
+    }
+    Complex& operator*=(const Complex& other) {
+        m_r = m_r*other.getReal() - m_i*other.getImag();
+        m_i = m_r*other.getImag() + m_i*other.getReal();
+        return *this;
+    }
+    Complex& operator/=(const Complex& other) {
+        m_r = (m_r*other.getReal() + m_i*other.getImag()) / (other.getReal()*other.getReal() + other.getImag()*other.getImag());
+        m_i = (m_i*other.getReal() - m_r*other.getImag()) / (other.getReal()*other.getReal() + other.getImag()*other.getImag());
+        return *this;
+    }
+    Complex operator+(const Complex& other) const {
+        return Complex(m_r + other.getReal(), m_i + other.getImag());
+    }
+    Complex operator-(const Complex& other) const {
+        return Complex(m_r - other.getReal(), m_i - other.getImag());
+    }
+    Complex operator*(const Complex& other) const {
+        return Complex(m_r * other.getReal() - m_i * other.getImag(),  //ahorramos variables xd
+                       m_r * other.getImag() + m_i * other.getReal());
+    }
+    Complex operator/(const Complex& other) const {
+        RealType denominator = other.getReal() * other.getReal() + other.getImag() * other.getImag();
+        
+        return Complex((m_r * other.getReal() + m_i * other.getImag()) / denominator,
+                       (m_i * other.getReal() - m_r * other.getImag()) / denominator);
+    }
 
+
+    Complex& operator =(const Complex& other) = default ;
+
+
+    //Conversion.
+    //   operator RealType() const { return m_r; }  
+    //   operator ImagType() const { return m_i; } 
+
+    // Getters
+    RealType getReal() const { return m_r; }
+    ImagType getImag() const { return m_i; }
+
+    //op out.
+    friend ostream& operator<<(ostream& os, const Complex& c) {
+         return os << c.getReal() << " + " << c.getImag() << "i";
+        
+    }
+    //op in.
+    friend istream& operator>>(istream& is, Complex& c) {
+        cout << "Enter real part: ";
+        is >> c.m_r;
+        cout << "Enter imaginary part: ";
+        is >> c.m_i;
+        return is;
+    }
+};
 #endif // __COMPLEX_H__
