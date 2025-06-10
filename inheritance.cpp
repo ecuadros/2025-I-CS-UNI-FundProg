79f810bbe861be0b5ae1afe090f9514ed2e6182f
FabricioYV <fabricioyauri0903@gmail.com>
2025-06-10 13:22:45 -0500

    finish work

--- inheritance.cpp
+++ inheritance.cpp
@@ -7,14 +7,11 @@
 using namespace std;
 
 void DemoInheritance(){
-    cout << "Demo Inheritance "<< endl;
-
-    Professor prof ("Dr. Smith", DocTypeDNI, "987654321", 5000.0);
-
-    cout <<endl;    
+    cout << "Demo Inheritance "<< endl;   
 
     GradStudent gradStudent("Alice", DocTypePassport, "912345123", "Computer Science", "AI Research");
-    
+    cout << endl;
+    Professor prof ("Dr. Smith", DocTypeDNI, "987654321", 5000.0);
 }
 
 
