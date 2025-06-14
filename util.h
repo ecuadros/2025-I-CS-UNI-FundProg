359c4325f576558afd185e2ae50472f491af7818
Diego Vasquez <diegoalonzovasquezmorales165@gmail.com>
2025-06-13 20:24:09 -0500

    cambio del bit

--- util.h
+++ util.h
@@ -1,5 +1,6 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
+#include<iostream>
 
 template <typename T>
 void intercambiar(T &a, T &b) {
@@ -12,5 +13,7 @@ int Suma    (int a, int b);
 int Resta   (int a, int b);
 int Mult    (int a, int b);
 int Division(int a, int b);
+
+int ChangeBit(int a,size_t n);
 
 #endif // __UTIL_H__
